b263839ea0e0c52f365bc720e28c8179e845cd60
Caio Carvalho <caio.cmalves@gmail.com>
2023-04-12 19:22:14 -0300

    Criando classes e instanciando objetos - aula 02b

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import classes.Caneta;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("testando git");
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5;
+        c1.tampada = false;
+        c1.modelo = "Bic Padrão";
+        c1.tampar();
+        c1.rabiscar();
+        c1.destampar();
+        c1.rabiscar();
+         
+       
+
     }
 }
---
+++ src/classes/Caneta.java
@@ -0,0 +1,36 @@
+package classes;
+
+public class Caneta {
+    public String modelo;
+    public String cor;
+    public double ponta;
+    public int carga;
+    public boolean tampada;
+
+    public void status(){
+        System.out.println("Modelo " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta:  " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+    public void rabiscar(){
+        if (this.tampada == true) {
+            System.out.println("ERRO: NÃO POSSO RABISCAR");            
+        }
+        else {
+            System.out.println("estou rabiscando!!");
+        }
+    }
+
+    public void tampar(){
+        this.tampada = true;
+    }
+
+    public void destampar(){
+        this.tampada = false;
+    }
+
+
+
+}
